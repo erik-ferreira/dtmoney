698027cef4de9cde6a6200cbf87a57c307cffcda
erik-ferreira <erikdesouzagf@gmail.com>
2022-05-27 22:21:29 -0300

    finish component dashboard and summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </div>
   );
